764f04e94f824a98226e75e2fa40f8b9ff6cbaa1
MukundSaiRathod <s572099@nwmissouri.edu>
2024-12-04 17:19:24 -0600

    updated login register pages

--- models/toys.js
+++ models/toys.js
@@ -1,9 +1,14 @@
 const mongoose = require("mongoose");
 
 const toySchema = mongoose.Schema({
-  name: String,
-  toy_type: String,
-  age_range: String
+  name: {type: String,required: true, 
+  },
+  toy_type: {type: String,required:true,
+  },
+  age_range: {type: String, required: true,  
+    minlength: [3, 'Age range should be at least 3 characters long'],
+    maxlength: [20, 'Age range should not exceed 20 characters'],  
+  },
 });
 
 module.exports = mongoose.model("Toy", toySchema);
--- routes/toys.js
+++ routes/toys.js
@@ -31,6 +31,14 @@
 var express = require('express');
 var router = express.Router();
 var toy_controller = require('../controllers/toys');
+// A little function to check if we have an authorized user and continue on or
+// redirect to login.
+const secured = (req, res, next) => {
+if (req.user){
+return next();
+}
+res.redirect("/login");
+}
 
 router.get('/', toy_controller.toy_list);
 
@@ -50,7 +58,7 @@ router.get('/detail', toy_controller.toy_view_one_Page);
 
 router.get('/create', toy_controller.toy_create_Page);
 
-router.get('/update', toy_controller.toy_update_Page);
+router.get('/update',secured,toy_controller.toy_update_Page);
 
 router.get('/delete', toy_controller.toy_delete_Page);
 
